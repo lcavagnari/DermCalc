f87f53710c48d791e458c7b9b7c04311c37ea4b1
Luca Cavagnari <luca.cavagnari03@proton.me>
2026-04-30 17:58:08 +0200

    initial commit: added DermCalc mockup visualizer

---
+++ docs/mockup/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import DermCalcMockup from "./DermCalcMockup2.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <DermCalcMockup />
+  </React.StrictMode>
+);
