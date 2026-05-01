f3ce84ac6013895ecea130ff132fcfecac0fce5a
Luca Cavagnari <luca.cavagnari03@proton.me>
2026-05-01 21:51:50 +0200

    added bottom nav bar logic

--- app/src/main/java/it/lcavagnari/pdm/dermcalc/navigation/BottomNavBar.kt
+++ app/src/main/java/it/lcavagnari/pdm/dermcalc/navigation/BottomNavBar.kt
@@ -1,2 +1,59 @@
 package it.lcavagnari.pdm.dermcalc.navigation
 
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination.Companion.hasRoute
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.currentBackStackEntryAsState
+
+@Composable
+fun BottomNavigationBar(navController: NavController, appItems: List<AppRoute>) {
+    NavigationBar(
+        containerColor = Color.White
+    ) {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentDestination = navBackStackEntry?.destination
+
+        appItems.forEach { item ->
+            // Using an explicit when block with hasRoute<T>() ensures the compiler uses
+            // the correct type-safe extension and avoids "restricted API" errors.
+            val isSelected = currentDestination
+                ?.hierarchy
+                ?.any {
+                    it.hasRoute(item::class)
+                } == true
+
+            NavigationBarItem(
+                icon = {
+                    Icon(
+                        imageVector = item.icon,
+                        contentDescription = item.name
+                    )
+                },
+                label = { Text(text = item.name?: "") },
+                selected = isSelected,
+                onClick = {
+                    navController.navigate(item) {
+                        // Pop up to the start destination of the graph to avoid building up a large stack
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+
+                        // Avoid multiple copies of the same destination when reselecting the same item
+                        launchSingleTop = true
+
+                        // Restore state when reselecting a previously selected item
+                        restoreState = true
+                    }
+                }
+            )
+        }
+    }
+}
