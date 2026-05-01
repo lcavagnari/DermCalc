506e2607055cdab20265eb77210c46c510d8101c
Luca Cavagnari <luca.cavagnari03@proton.me>
2026-05-01 21:50:18 +0200

    updated screens code

--- app/src/main/java/it/lcavagnari/pdm/dermcalc/screens/ProfileScreen.kt
+++ app/src/main/java/it/lcavagnari/pdm/dermcalc/screens/ProfileScreen.kt
@@ -1,2 +1,30 @@
 package it.lcavagnari.pdm.dermcalc.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+
+@Composable
+fun LoginScreen(navController: NavHostController) {
+    Column(
+        modifier = Modifier.fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+    ) {
+        Text(
+            text = "Profile",
+            fontSize = 18.sp,
+            color = Color.Gray,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center
+        )
+    }
+}
