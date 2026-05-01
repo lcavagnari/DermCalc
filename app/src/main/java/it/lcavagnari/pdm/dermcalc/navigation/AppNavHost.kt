86120872b8dc3435075519d077a3825741b4d8e4
Luca Cavagnari <luca.cavagnari03@proton.me>
2026-05-01 21:56:59 +0200

    added navhost

--- app/src/main/java/it/lcavagnari/pdm/dermcalc/navigation/AppNavHost.kt
+++ app/src/main/java/it/lcavagnari/pdm/dermcalc/navigation/AppNavHost.kt
@@ -1,2 +1,36 @@
 package it.lcavagnari.pdm.dermcalc.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import it.lcavagnari.pdm.dermcalc.screens.HomeScreen
+import it.lcavagnari.pdm.dermcalc.screens.ProfileRoute
+import it.lcavagnari.pdm.dermcalc.screens.ToolsScreen
+
+@Composable
+fun AppNavHost(
+    modifier: Modifier = Modifier,
+    navController: NavHostController,
+    startDestination: AppRoute = HomeRoute
+) {
+    NavHost(
+        modifier = modifier,
+        navController = navController,
+        startDestination = startDestination
+    ) {
+        composable<HomeRoute> {
+            HomeScreen(navController)
+
+        }
+
+        composable<ToolsRoute> {
+            ToolsScreen(navController)
+        }
+
+        composable<ProfileRoute> {
+            ProfileRoute(navController)
+        }
+    }
+}
