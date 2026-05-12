70783f69412acd8b785309ed42b6475f770a1e54
Luca Cavagnari <luca.cavagnari03@proton.me>
2026-05-12 16:18:34 +0200

    ref(landscape): modified warning screen

--- app/src/main/java/it/lcavagnari/pdm/dermcalc/ui/landscape/MainLandscapeActivity.kt
+++ app/src/main/java/it/lcavagnari/pdm/dermcalc/ui/landscape/MainLandscapeActivity.kt
@@ -1,23 +1,44 @@
 package it.lcavagnari.pdm.dermcalc.ui.landscape
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import it.lcavagnari.pdm.dermcalc.R
 import it.lcavagnari.pdm.dermcalc.models.OnboardingModel
 
 @Composable
 fun MainLandscapeActivity(onboardingModel: OnboardingModel) {
 
-    Box(modifier = Modifier
-        .fillMaxSize()
-        .wrapContentSize()) {
+    Column(
+        modifier = Modifier.fillMaxSize()
+        .wrapContentSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Icon(
+            modifier = Modifier.size(200.dp),
+            painter = painterResource(id = R.drawable.ic_ecg),
+            contentDescription = "App Logo",
+            tint = MaterialTheme.colorScheme.primary
+        )
+
         Text(
-            "DermCalc does not support Landscape yet, flip the device to vertical mode in order to continue using the app. No data was lost.",
-            textAlign = TextAlign.Center
+            "DermCalc does not support Landscape yet.\nFlip the device to vertical mode in order to continue using the app.\n\n No data was lost.",
+            style = MaterialTheme.typography.bodyLarge,
+            color = MaterialTheme.colorScheme.onSurfaceVariant
         )
     }
 
