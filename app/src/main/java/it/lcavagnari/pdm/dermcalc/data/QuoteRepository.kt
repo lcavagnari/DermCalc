6e34d07859348056e986774657bd4d1438971706
Luca Cavagnari <luca.cavagnari03@proton.me>
2026-05-13 11:09:32 +0200

    feat(data): add QuoteRepository with Quote data class and getRandomQuote

--- app/src/main/java/it/lcavagnari/pdm/dermcalc/data/QuoteRepository.kt
+++ app/src/main/java/it/lcavagnari/pdm/dermcalc/data/QuoteRepository.kt
@@ -1,22 +1,20 @@
-package it.lcavagnari.pdm.dermcalc.models
+package it.lcavagnari.pdm.dermcalc.data
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringArrayResource
-import androidx.compose.ui.res.stringResource
 import it.lcavagnari.pdm.dermcalc.R
 import kotlinx.serialization.Serializable
-import kotlin.random.Random
-
 
 
 @Composable
-fun getRandomQuote(): Quote {
+public fun getRandomQuote(): Quote {
     val quotes = stringArrayResource(id = R.array.home_quotes)
     val quoteRaw = quotes.random().split(" — ")
 
     return Quote(
         quote = quoteRaw[0],
-        author = if (quoteRaw.size > 1 && quoteRaw[1].isNotBlank() && !quoteRaw[1].equals("Unknown")) quoteRaw[1] else null
+        author = if (quoteRaw.size > 1 && quoteRaw[1].isNotBlank()
+                        && quoteRaw[1] != "Unknown") quoteRaw[1] else null
     )
 }
 
