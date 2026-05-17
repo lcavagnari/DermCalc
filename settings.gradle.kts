5533154b043a7517848653340b66e3e55bb6c1a8
Luca Cavagnari <luca.cavagnari03@proton.me>
2026-05-17 10:21:13 +0200

    Fix Gradle plugin resolution in CI

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,9 +10,18 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
-}
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+    resolutionStrategy {
+        eachPlugin {
+            when (requested.id.id) {
+                "com.android.application" -> useModule("com.android.tools.build:gradle:${requested.version}")
+                "org.jetbrains.kotlin.android",
+                "org.jetbrains.kotlin.plugin.compose",
+                "org.jetbrains.kotlin.plugin.serialization" -> useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:${requested.version}")
+                "com.google.devtools.ksp" -> useModule("com.google.devtools.ksp:symbol-processing-gradle-plugin:${requested.version}")
+                "org.owasp.dependencycheck" -> useModule("org.owasp:dependency-check-gradle:${requested.version}")
+            }
+        }
+    }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
