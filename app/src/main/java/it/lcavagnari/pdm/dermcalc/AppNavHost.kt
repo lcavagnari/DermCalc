9ea55e2e9a0bda4a6a7200d9a30ba3052dcc0eaa
Luca Cavagnari <luca.cavagnari03@proton.me>
2026-05-19 17:05:19 +0200

    refactor(component): move BottomNavBar from navigation to component package

--- app/src/main/java/it/lcavagnari/pdm/dermcalc/AppNavHost.kt
+++ app/src/main/java/it/lcavagnari/pdm/dermcalc/AppNavHost.kt
@@ -1,4 +1,4 @@
-package it.lcavagnari.pdm.dermcalc.navigation
+package it.lcavagnari.pdm.dermcalc.ui.
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -8,9 +8,18 @@ import androidx.navigation.compose.composable
 import it.lcavagnari.pdm.dermcalc.models.OnboardingModel
 import it.lcavagnari.pdm.dermcalc.models.QuoteModel
 import it.lcavagnari.pdm.dermcalc.models.ToolsModel
+import it.lcavagnari.pdm.dermcalc.navigation.AppRoute
+import it.lcavagnari.pdm.dermcalc.navigation.BMIToolRoute
+import it.lcavagnari.pdm.dermcalc.navigation.BSAToolRoute
+import it.lcavagnari.pdm.dermcalc.navigation.EASIToolRoute
+import it.lcavagnari.pdm.dermcalc.navigation.HomeRoute
+import it.lcavagnari.pdm.dermcalc.navigation.PASIToolRoute
+import it.lcavagnari.pdm.dermcalc.navigation.ProfileRoute
+import it.lcavagnari.pdm.dermcalc.navigation.ToolsRoute
 import it.lcavagnari.pdm.dermcalc.ui.portrait.screens.HomeScreen
 import it.lcavagnari.pdm.dermcalc.ui.portrait.screens.ProfileRoute
 import it.lcavagnari.pdm.dermcalc.ui.portrait.screens.ToolsScreen
+
 
 /**
  * Registers destination composables and wires type-safe route navigation.
@@ -38,15 +47,14 @@ fun AppNavHost(
     ) {
         composable<HomeRoute> {
             HomeScreen(navController, quoteModel, onboardingModel,toolsModel)
-
         }
 
-        composable<ToolsRoute> {
-            ToolsScreen(navController, toolsModel)
-        }
+        composable<ToolsRoute> { ToolsScreen(navController, toolsModel) }
+        composable<ProfileRoute> { ProfileRoute(navController, onboardingModel) }
 
-        composable<ProfileRoute> {
-            ProfileRoute(navController, onboardingModel)
-        }
+        composable<BMIToolRoute>  { ToolsScreen(navController, toolsModel) }
+        composable<BSAToolRoute>  { ToolsScreen(navController, toolsModel) }
+        composable<PASIToolRoute> { ToolsScreen(navController, toolsModel) }
+        composable<EASIToolRoute> { ToolsScreen(navController, toolsModel) }
     }
 }
