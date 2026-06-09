8f654db8af97d241230248a30d492083cbd789fc
Luca Cavagnari <luca.cavagnari03@proton.me>
2026-06-09 10:40:26 +0200

    fix(unit): fixed inialisation issue in OnboardingModelTest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -128,6 +128,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.appcompat)
+    implementation(libs.core.ktx)
     testImplementation(libs.junit)
     testImplementation(libs.robolectric)
     androidTestImplementation(libs.androidx.junit)
--- app/src/test/java/it/lcavagnari/pdm/dermcalc/models/OnboardingModelTest.kt
+++ app/src/test/java/it/lcavagnari/pdm/dermcalc/models/OnboardingModelTest.kt
@@ -10,8 +10,10 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
+@Config(sdk = [34])
 class OnboardingModelTest {
 
     private lateinit var model: OnboardingModel
