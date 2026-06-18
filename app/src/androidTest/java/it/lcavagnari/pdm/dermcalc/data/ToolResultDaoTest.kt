f26df2b2aefc541b8d53c71efcb13af23632ba3a
Luca Cavagnari <luca.cavagnari03@proton.me>
2026-06-18 12:46:05 +0200

    Add AGPL-3.0 copyright header to all Kotlin source files

--- app/src/androidTest/java/it/lcavagnari/pdm/dermcalc/data/ToolResultDaoTest.kt
+++ app/src/androidTest/java/it/lcavagnari/pdm/dermcalc/data/ToolResultDaoTest.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2026 Luca Cavagnari
+ *
+ * This file is part of DermCalc, final project for the Mobile Device Programming course of Univerità Degli Studi Dell'Insubria.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ */
 package it.lcavagnari.pdm.dermcalc.data
 
 import android.content.Context
--- app/src/main/java/it/lcavagnari/pdm/dermcalc/ui/screens/ProfileScreen.kt
+++ app/src/main/java/it/lcavagnari/pdm/dermcalc/ui/screens/ProfileScreen.kt
@@ -1,4 +1,19 @@
-﻿package it.lcavagnari.pdm.dermcalc.ui.screens
+/*
+ * Copyright (C) 2026 Luca Cavagnari
+ *
+ * This file is part of DermCalc, final project for the Mobile Device Programming course of Univerità Degli Studi Dell'Insubria.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ */
+package it.lcavagnari.pdm.dermcalc.ui.screens
 
 import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.animateFloat
